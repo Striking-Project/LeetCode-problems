01cc459ac7fbacd5a9262488b80ca399be16acd9
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-11 13:13:51 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 238
            Language: Python3
            Runtime: 162 ms
            Runtime percentile: 81.34
            Memory: 23.748 MB
            Memory percentile: 88.72

--- 238. Product of Array Except Self/Solution.py
+++ 238. Product of Array Except Self/Solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        prefix_product = [1] * n
-        suffix_product = [1] * n
-        result = [0] * n
-
-        for i in range(1, n):
-            prefix_product[i] = prefix_product[i - 1] * nums[i - 1] 
+        result = [1] * n
         
-        for i in range(n - 2, -1, -1):
-            suffix_product[i] = suffix_product[i + 1] * nums[i + 1]
-        
+        prefix_product = 1
         for i in range(n):
-            result[i] = prefix_product[i] * suffix_product[i]
+            result[i] *= prefix_product
+            prefix_product *= nums[i]
+        
+        suffix_product = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= suffix_product
+            suffix_product *= nums[i]
         
         return result
