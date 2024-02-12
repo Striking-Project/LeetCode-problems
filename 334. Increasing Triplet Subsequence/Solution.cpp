b56a15c8a4a13ed80ccfe647bec48cc6bece4780
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-12 21:33:48 +0100

    C++ Solution
            Submission Statistics:
            Question #: 334
            Language: C++
            Runtime: 75 ms
            Runtime percentile: 95.54
            Memory: 114.12 MB
            Memory percentile: 29.48

---
+++ 334. Increasing Triplet Subsequence/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first_min = INT_MAX;
+        int second_min = INT_MAX;
+
+        for (int num : nums) {
+            if (num <= first_min) {
+                first_min = num;
+            } else if (num <= second_min) {
+                second_min = num;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
