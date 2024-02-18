7120f9ab5ece31cbc05a9e31ff78483282fea10b
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-18 15:40:59 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 643
            Language: Python3
            Runtime: 920 ms
            Runtime percentile: 36.52
            Memory: 28.408 MB
            Memory percentile: 71.45

--- 643. Maximum Average Subarray I/Solution.py
+++ 643. Maximum Average Subarray I/Solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        window_sum = sum(nums[:k])
-        max_sum = window_sum
-        
+        max_sum = window_sum = sum(nums[:k])
+                
         for i in range(k, len(nums)):
             window_sum += nums[i] - nums[i - k]
             max_sum = max(max_sum, window_sum)
