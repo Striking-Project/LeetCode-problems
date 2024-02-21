34f1b23706bd3018d065a96ba8090396625796f2
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-21 18:17:30 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 1833
            Language: Python3
            Runtime: 40 ms
            Runtime percentile: 50.99
            Memory: 16.568 MB
            Memory percentile: 69.47

--- 1833. Find the Highest Altitude/Solution.py
+++ 1833. Find the Highest Altitude/Solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        altitude = 0
         max_altitude = 0
+        current_altitude = 0
         
-        for g in gain:
-            altitude += g
+        prefix_sum = [0] * (len(gain) + 1)
+        for i in range(1, len(gain) + 1):
+            prefix_sum[i] = prefix_sum[i - 1] + gain[i - 1]
+        
+        for altitude in prefix_sum:
             max_altitude = max(max_altitude, altitude)
         
         return max_altitude
