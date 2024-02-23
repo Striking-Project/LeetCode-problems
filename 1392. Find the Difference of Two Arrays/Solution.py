51e58b1f84c499a9e7a8b56b34d679ea4908ce8d
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-23 15:27:50 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 1392
            Language: Python3
            Runtime: 145 ms
            Runtime percentile: 52.02
            Memory: 17.116 MB
            Memory percentile: 58.34

--- 1392. Find the Difference of Two Arrays/Solution.py
+++ 1392. Find the Difference of Two Arrays/Solution.py
@@ -1,9 +1,23 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        set1 = set(nums1)
-        set2 = set(nums2)
+        freq1 = {}
+        freq2 = {}
         
-        difference1 = list(set1 - set2)
-        difference2 = list(set2 - set1)
+        for num in nums1:
+            freq1[num] = freq1.get(num, 0) + 1
+        
+        for num in nums2:
+            freq2[num] = freq2.get(num, 0) + 1
+        
+        difference1 = []
+        difference2 = []
+        
+        for num in freq1:
+            if num not in freq2:
+                difference1.append(num)
+        
+        for num in freq2:
+            if num not in freq1:
+                difference2.append(num)
         
         return [difference1, difference2]
