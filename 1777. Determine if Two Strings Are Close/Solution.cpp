4b95314bbc5def987a9ab4d5e44b89a1863395ec
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-25 15:59:20 +0100

    C++ Solution
            Submission Statistics:
            Question #: 1777
            Language: C++
            Runtime: 116 ms
            Runtime percentile: 26.76
            Memory: 23.2 MB
            Memory percentile: 5.26

--- 1777. Determine if Two Strings Are Close/Solution.cpp
+++ 1777. Determine if Two Strings Are Close/Solution.cpp
@@ -5,15 +5,16 @@ public:
         for (char ch : word1) freq1[ch]++;
         for (char ch : word2) freq2[ch]++;
         
-        set<char> chars1(word1.begin(), word1.end()), chars2(word2.begin(), word2.end());
-        
         vector<int> freq1Values, freq2Values;
-        for (auto& kv : freq1) freq1Values.push_back(kv.second);
+        for (auto& kv : freq1) {
+            if (!freq2.count(kv.first)) return false;
+            freq1Values.push_back(kv.second);
+        }
         for (auto& kv : freq2) freq2Values.push_back(kv.second);
         
         sort(freq1Values.begin(), freq1Values.end());
         sort(freq2Values.begin(), freq2Values.end());
         
-        return chars1 == chars2 && freq1Values == freq2Values;
+        return freq1Values == freq2Values;
     }
 };
