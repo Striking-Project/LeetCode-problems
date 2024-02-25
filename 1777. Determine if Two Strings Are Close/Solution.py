4dff8a299f956d09848a19f86756c8ee6002d8d7
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-25 15:28:16 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 1777
            Language: Python3
            Runtime: 148 ms
            Runtime percentile: 47.91
            Memory: 17.444 MB
            Memory percentile: 97.06

---
+++ 1777. Determine if Two Strings Are Close/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        
+        freq1 = collections.Counter(word1)
+        freq2 = collections.Counter(word2)
+        
+        if set(freq1.keys()) != set(freq2.keys()):
+            return False
+
+        return sorted(freq1.values()) == sorted(freq2.values())
