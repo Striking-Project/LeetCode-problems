fd7da1b395ea3abb32c0b32380c3cf6a6bf1f7e6
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-27 18:19:44 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 2470
            Language: Python3
            Runtime: 131 ms
            Runtime percentile: 75.80
            Memory: 18.62 MB
            Memory percentile: 38.53

--- 2470. Removing Stars From a String/Solution.py
+++ 2470. Removing Stars From a String/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def removeStars(self, s: str) -> str:
         stack = []
         for char in s:
-            if char == '*':
+            if char is '*':
                 if stack:
                     stack.pop()
             else:
