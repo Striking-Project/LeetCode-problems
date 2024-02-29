54d44f5f1ea44190c07527b888b8f81c703fef2f
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-02-29 15:41:23 +0100

    Python3 Solution
            Submission Statistics:
            Question #: 394
            Language: Python3
            Runtime: 27 ms
            Runtime percentile: 93.64
            Memory: 16.676 MB
            Memory percentile: 41.62

--- 394. Decode String/Solution.py
+++ 394. Decode String/Solution.py
@@ -8,11 +8,13 @@ class Solution:
             if char.isdigit():
                 current_num = current_num * 10 + int(char)
             elif char == '[':
-                stack.append((current_string, current_num))
+                stack.append(current_string)
+                stack.append(current_num)
                 current_string = ''
                 current_num = 0
             elif char == ']':
-                prev_string, num = stack.pop()
+                num = stack.pop()
+                prev_string = stack.pop()
                 current_string = prev_string + current_string * num
             else:
                 current_string += char
