6b89eef31a813a1e38eba7aa323fafc29767f6e1
Striking-Porject <mohamed.benabdallah@isticbc.org>
2024-03-03 16:46:54 +0100

    C++ - Runtime : 98ms/94.71% - Memory : 61.09MB/42.03%

---
+++ 933. Number of recent Calls/solution.cpp
@@ -0,0 +1,23 @@
+#include <queue>
+
+class RecentCounter {
+public:
+    RecentCounter() {}
+
+    int ping(int t) {
+        while (!requests.empty() && requests.front() < t - 3000) {
+            requests.pop();
+        }
+        requests.push(t);
+        return requests.size();
+    }
+
+private:
+    std::queue<int> requests;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
