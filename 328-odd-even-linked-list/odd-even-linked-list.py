5cc6c894bf0b2060d1c16ac9bc007ad56bb89dbf
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-03-06 09:58:20 +0100

    Time: 42 ms (44.36%) | Memory: 18.5 MB (81.78%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -14,10 +14,8 @@ class Solution:
         even_tail = even_head
         
         while even_tail and even_tail.next:
-            odd_tail.next = even_tail.next
-            odd_tail = odd_tail.next
-            even_tail.next = odd_tail.next
-            even_tail = even_tail.next
+            odd_tail.next, odd_tail = even_tail.next, even_tail.next
+            even_tail.next, even_tail = odd_tail.next, odd_tail.next
         
         odd_tail.next = even_head
         
