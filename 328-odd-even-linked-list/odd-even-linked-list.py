98892f80f605c6d6e35890f848a51ccaf0cda381
Striking-Porject <mohamed.benabdallah@isticbc.org>
2024-03-06 10:16:00 +0100

    Python3 - Runtime : 33ms/91.45% - Memory : 18.45MB/98.47%

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
