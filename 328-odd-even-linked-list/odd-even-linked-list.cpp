041bbdff573051ec3638d75b10baaebb17e3c126
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-03-06 10:08:46 +0100

    Time: 8 ms (62.10%) | Memory: 13.9 MB (74.62%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }        
+
+        ListNode* odd_head = head;
+        ListNode* even_head = head->next;
+        ListNode* odd_tail = odd_head;
+        ListNode* even_tail = even_head;
+
+        while (even_tail && even_tail->next) {
+            odd_tail->next = even_tail->next;
+            odd_tail = odd_tail->next;
+            even_tail->next = odd_tail->next;
+            even_tail = even_tail->next;
+        }
+
+        odd_tail->next = even_head;
+
+        return odd_head;
+    }
+};
