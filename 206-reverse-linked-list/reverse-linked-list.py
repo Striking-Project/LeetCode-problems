935e0ae99f9b328fd1e5e60d330cac81d63d9609
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-03-07 09:05:48 +0100

    Time: 35 ms (74.07%) | Memory: 17.7 MB (49.08%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -3,13 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Recursive :)
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+#         
+#         reverse_head = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#
+#         return reverse_head
+
+# Iterative :)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        reverse_head = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-
-        return reverse_head
+        prev = None
+        current = head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+            
+        return prev
