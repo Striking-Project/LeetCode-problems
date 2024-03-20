d68fdeea37de85080470891c7d4204bae773c53b
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-03-20 15:24:14 +0100

    Time: 398 ms (39.45%) | Memory: 121.1 MB (45.99%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -4,17 +4,19 @@ public:
         visited[currCity] = true;
         for (auto neighbourCity : adj[currCity]) {
             if (!visited[neighbourCity.first]) {
-                if (neighbourCity.second == 1)
+                if (neighbourCity.second == 1) {
                     minChange++;
+                }
                 dfs(adj, visited, minChange, neighbourCity.first);
             }
         }
     }
+    
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> adj(n);
         for (auto connection : connections) {
-            adj[connection[0]].push_back({connection[1], 1}); // city away from 0
-            adj[connection[1]].push_back({connection[0], -1}); // city toward 0
+            adj[connection[0]].push_back({connection[1], 1});
+            adj[connection[1]].push_back({connection[0], -1});
         }
         vector<bool> visited(n, false);
         int minChange = 0;
