0e04938acfb22a8af6b847da3e4d743ebedc0eca
Mohamed Ben Abdallah <80620671+Striking-Project@users.noreply.github.com>
2024-03-21 15:41:28 +0100

    Time: 32 ms (81.95%) | Memory: 16.6 MB (54.15%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(dict)
+        
+        # Construct the graph
+        for (Ai, Bi), value in zip(equations, values):
+            graph[Ai][Bi] = value
+            graph[Bi][Ai] = 1 / value
+        
+        def dfs(start, end, visited):
+            if start not in graph or end not in graph:
+                return -1.0
+            
+            if start == end:
+                return 1.0
+            
+            visited.add(start)
+            for neighbor, value in graph[start].items():
+                if neighbor not in visited:
+                    result = dfs(neighbor, end, visited)
+                    if result != -1.0:
+                        return value * result
+            
+            return -1.0
+        
+        results = []
+        for query in queries:
+            result = dfs(query[0], query[1], set())
+            results.append(result)
+        
+        return results
